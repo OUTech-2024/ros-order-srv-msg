ec568beefea0ba0cccf1d2e5abc873ebb285091a
sudogauss <tsimafei.liashkevich@telecom-sudparis.eu>
2023-10-18 11:16:27 +0200

    chore: Basic srv and msg layout through cmake. Update gitignore with IDE definition (vscode). Move a part of cmake to cmake files. package.xml for ROS2

---
+++ cmake/declarations.cmake
@@ -0,0 +1,6 @@
+#########== Cmake C/C++ standard ==#########
+set(CMAKE_C_STANDARD 99)
+set(CMAKE_CXX_STANDARD 17)
+
+#########== Compiler flags (gcc) ==#########
+add_compile_options(-Wall -Wextra -Wpedantic)
---
+++ cmake/dependencies.cmake
@@ -0,0 +1,3 @@
+#########== ROS dependencies ==#########
+find_package(ament_cmake REQUIRED)
+find_package(rosidl_default_generators REQUIRED)
